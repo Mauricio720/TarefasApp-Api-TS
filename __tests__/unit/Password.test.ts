d7ec1cb8e5066889cfebd789c6aed03412e3fcfa
mauricio ferreira <mauricio-ferreira2015@outlook.com>
2024-02-13 15:51:47 -0300

    refactory-test: domain user value objects

--- __tests__/unit/Password.test.ts
+++ __tests__/unit/Password.test.ts
@@ -1,14 +1,18 @@
-import { Encrypt } from "application/security/Encrypt";
+import { Encrypt, EncryptPassword } from "application/security/Encrypt";
 import { Password } from "domain/valueObject/Password";
 
-const encryptPass: Encrypt = {
-  encrypt: jest.fn().mockReturnValue("any_password"),
-  check: jest.fn().mockReturnValue(true),
+const encryptPass: EncryptPassword = {
+  encrypt: jest.fn().mockReturnValue("any_password")
 }
 
 test("should create password", () => {
   const password = new Password("any",encryptPass);
   expect(password.getValue()).toBe("any_password");
+})
+
+test("should check password", () => {
+  const password = new Password("any1234");
+  expect(password.getValue()).toBe("any1234");
 })
 
 test("should throw error required password", () => {
