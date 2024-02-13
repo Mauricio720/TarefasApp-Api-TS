d2ba2af36657727d25571a360a97e3730e6067c9
mauricio ferreira <mauricio-ferreira2015@outlook.com>
2024-02-13 15:54:36 -0300

    test: create user use case

---
+++ __tests__/integration/CreateUser.test.ts
@@ -0,0 +1,26 @@
+import { UserRepository } from "application/repository/UserRepository";
+import { CreateUser } from "application/useCases/CreateUser";
+import { UserRepositoryMemory } from "infra/repository/memory/UserRepositoryMemory";
+
+describe("Create User", () => {
+  let userRepository:UserRepository;
+
+  beforeEach(() => {
+    userRepository = new UserRepositoryMemory()
+  })
+  
+  test("should create user", async () => {
+    const inputCreateUser={
+      name: 'any name',
+      thumbnail:'any',
+      email: 'any@any.com',
+      login: 'any',
+      password:'1234'
+    }
+    const createUser=new CreateUser(userRepository)
+    const id=await createUser.execute(inputCreateUser)
+    
+    const user = await userRepository.findById(id)
+    expect(user.getId()).toBe(id)
+  })
+})
